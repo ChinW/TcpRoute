24ce23a1b271488085c1f699fc2d5ed747bfbbf9
GameXG <GameXG@Gmail.Com>
2015-08-10 10:41:00 +0800

    0.0.0.0 时不绑定地址

--- upstream/direct.py
+++ upstream/direct.py
@@ -22,33 +22,37 @@ class DirectUpstream(UpstreamBase):
         u""" 初始化直连 socket 环境 """
         UpstreamBase.__init__(self,config=config)
 
-        self.source_ip = config.get('source_ip','0.0.0.0')
-        self.source_port = config.get('source_port',0)
+        source_ip = config.get('source_ip','0.0.0.0')
+        source_port = config.get('source_port',0)
+
+        if source_ip == '0.0.0.0' and source_port==0:
+            self.source_address = None
+        else:
+            self.source_address=(source_ip,source_port)
 
         class socket(SocketBase):
             def __init__(self, family=_socket.AF_INET, type=_socket.SOCK_STREAM, proto=0,_sock=None):
                 if _sock is None:
                     _sock = socket.upsocket.socket(family=family,type=type,proto=proto)
-                    _sock.bind((socket.source_ip,socket.source_port))
+                    _sock.bind(source_ip.source_addres)
                 SocketBase.__init__(self,_sock)
 
-        socket.source_ip = self.source_ip
-        socket.source_port = self.source_port
+        socket.source_addres = self.source_address
         socket.upstream = self.upstream
 
         self.socket = socket
 
     def get_display_name(self):
-        return '[%s]source_ip=%s,source_port=%s' % (self.type,self.source_ip,self.source_port)
+        return '[%s]source_ip=%s,source_port=%s' % (self.type,self.source_addres[0],self.source_addres[1])
 
     def get_name(self):
-        return '%s?source_ip=%s&source_port=%s' % (self.type,self.source_ip,self.source_port)
+        return '%s?source=%s&source_port=%s' % (self.type,self.source_addres[0],self.source_addres[1])
 
-    def create_connection(self,address, timeout=10, data_timeout = 5*60):
+    def create_connection(self,address, timeout=10):
         # TODO: 这里需要记录下本sock连接远程的耗时。
         # TODO: 需要实现多ip同时连接的功能。
-        _sock = self.upstream.create_connection(address,timeout,source_address=(self.source_ip,self.source_port))
+
+        _sock = self.upstream.create_connection(address,timeout,self.source_address)
         sock = self.socket(_sock=_sock)
         _sock.setsockopt(_socket.IPPROTO_TCP,_socket.TCP_NODELAY, 1)
-        _sock.settimeout(data_timeout)
         return sock
