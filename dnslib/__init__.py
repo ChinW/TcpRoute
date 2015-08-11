950b94d191aa3f243a95041651df0add1eaa1d57
GameXG <GameXG@Gmail.Com>
2015-08-11 13:08:16 +0800

    直连时使用多IP同时连接

--- dnslib/__init__.py
+++ dnslib/__init__.py
@@ -10,22 +10,37 @@ u''' DNS 部分，主要的方案有：
  '''
 import logging
 import traceback
-import dns
 from gevent import socket
 import gevent
 import time
 import re
+from gevent.threadpool import ThreadPool
+import sys
 from LRUCacheDict import lru_cache
 
-
+try:
+    import dns.resolver
+    import dns.rdtypes.IN.A
+    import dns.rdtypes.IN.AAAA
+    import dns.rdtypes.ANY.CNAME
+except:
+    print >>sys.stderr, 'please install dnspython !'
+    sys.exit(1)
 
 ATYP_IPV4 = 0x01
 ATYP_DOMAINNAME = 0x03
 ATYP_IPV6 = 0x04
 
-
 RE_IPV4_ADDR = re.compile(r'^((25[0-5]|2[0-4]\d|[01]?\d\d?)\.){3}(25[0-5]|2[0-4]\d|[01]?\d\d?)$')
-RE_IPV6_ADDR = re.compile(r'^\s*((([0-9A-Fa-f]{1,4}:){7}([0-9A-Fa-f]{1,4}|:))|(([0-9A-Fa-f]{1,4}:){6}(:[0-9A-Fa-f]{1,4}|((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){5}(((:[0-9A-Fa-f]{1,4}){1,2})|:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){4}(((:[0-9A-Fa-f]{1,4}){1,3})|((:[0-9A-Fa-f]{1,4})?:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){3}(((:[0-9A-Fa-f]{1,4}){1,4})|((:[0-9A-Fa-f]{1,4}){0,2}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){2}(((:[0-9A-Fa-f]{1,4}){1,5})|((:[0-9A-Fa-f]{1,4}){0,3}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){1}(((:[0-9A-Fa-f]{1,4}){1,6})|((:[0-9A-Fa-f]{1,4}){0,4}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(:(((:[0-9A-Fa-f]{1,4}){1,7})|((:[0-9A-Fa-f]{1,4}){0,5}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:)))(%.+)?\s*$')
+RE_IPV6_ADDR = re.compile(
+    r'^\s*((([0-9A-Fa-f]{1,4}:){7}([0-9A-Fa-f]{1,4}|:))|(([0-9A-Fa-f]{1,4}:){6}(:[0-9A-Fa-f]{1,4}|((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){5}(((:[0-9A-Fa-f]{1,4}){1,2})|:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){4}(((:[0-9A-Fa-f]{1,4}){1,3})|((:[0-9A-Fa-f]{1,4})?:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){3}(((:[0-9A-Fa-f]{1,4}){1,4})|((:[0-9A-Fa-f]{1,4}){0,2}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){2}(((:[0-9A-Fa-f]{1,4}){1,5})|((:[0-9A-Fa-f]{1,4}){0,3}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){1}(((:[0-9A-Fa-f]{1,4}){1,6})|((:[0-9A-Fa-f]{1,4}){0,4}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(:(((:[0-9A-Fa-f]{1,4}){1,7})|((:[0-9A-Fa-f]{1,4}){0,5}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:)))(%.+)?\s*$')
+
+dnsPool = ThreadPool(20)
+configIpBlacklist = []
+errIP = {}
+nameservers = 'system'
+nameservers_backup = ['8.8.8.8', '208.67.222.222']
+
 
 def get_addr_type(addr):
     u""" 分析地址类型(IPv4、IPv6、域名)
@@ -74,53 +89,55 @@ True
         return ATYP_DOMAINNAME
 
 
-
 if __name__ == "__main__":
     import doctest
+
     doctest.testmod()
 
 
-
-u"""
-
-@lru_cache(500,10*60*1000,lock=None)
+@lru_cache(500, 10 * 60 * 1000, lock=None)
 def dnsQuery(hostname):
     global errIP
 
-    ipList = _dnsQuery(hostname,nameservers)
+    ipList = _dnsQuery(hostname, nameservers)
 
     for ip in ipList:
         if ip in errIP:
             # 解析异常，清空解析结果
-            logging.info(u'[DNS]默认解析服务器解析到异常IP，hostname=%s ，ip=%s ,nameserver=%s'%(hostname,ip,nameservers))
-            ipList=[]
+            logging.info(u'[DNS]默认解析服务器解析到异常IP，hostname=%s ，ip=%s ,nameserver=%s' % (hostname, ip, nameservers))
+            ipList = []
             break
 
     if not ipList:
         # 无解析结果时使用备用服务器重新解析
         for i in range(4):
-            tcp = bool((i+1)%2) # 间隔使用 TCP 协议查询
-            ipList = _dnsQuery(hostname,nameserversBackup,tcp)
+            tcp = bool((i + 1) % 2)  # 间隔使用 TCP 协议查询
+            ipList = _dnsQuery(hostname, nameservers_backup, tcp)
             for ip in ipList:
                 if ip in errIP:
-                    ipList=[]
-                    logging.info(u'[DNS]备用解析服务器解析得到异常IP(%s)，hostname=%s ，ip=%s ,nameserver=%s,TCP=%s'%(i,hostname,ip,nameserversBackup,tcp))
+                    ipList = []
+                    logging.info(u'[DNS]备用解析服务器解析得到异常IP(%s)，hostname=%s ，ip=%s ,nameserver=%s,TCP=%s' % (
+                        i, hostname, ip, nameservers_backup, tcp))
                     break
             if ipList:
-                logging.debug(u'[DNS]备用解析服务器解析成功(%s)，hostname=%s ，ip=%s ,nameserver=%s,TCP=%s'%(i,hostname,ipList,nameserversBackup,tcp))
+                logging.debug(u'[DNS]备用解析服务器解析成功(%s)，hostname=%s ，ip=%s ,nameserver=%s,TCP=%s' % (
+                    i, hostname, ipList, nameservers_backup, tcp))
                 return ipList
             else:
-                logging.info(u'[DNS]备用解析服务器解析失败(%s)，hostname=%s ，nameserver=%s,TCP=%s'%(i,hostname,nameserversBackup,tcp))
+                logging.info(
+                    u'[DNS]备用解析服务器解析失败(%s)，hostname=%s ，nameserver=%s,TCP=%s' % (i, hostname, nameservers_backup, tcp))
 
     else:
-        logging.debug(u'[DNS]默认解析服务器解析成功，hostname=%s ，ip=%s ,nameserver=%s'%(hostname,ipList,nameservers))
+        logging.debug(u'[DNS]默认解析服务器解析成功，hostname=%s ，ip=%s ,nameserver=%s' % (hostname, ipList, nameservers))
 
     if not ipList:
-        logging.warn(u'[DNS]默认及备用解析服务器解析失败，hostname=%s ，ip=%s ,nameserver=%s ,nameserversBackup=%s'%(hostname,ipList,nameservers,nameserversBackup))
+        logging.warn(u'[DNS]默认及备用解析服务器解析失败，hostname=%s ，ip=%s ,nameserver=%s ,nameserversBackup=%s' % (
+            hostname, ipList, nameservers, nameservers_backup))
 
     return ipList
 
-def _dnsQuery(hostname,serveListr='system',tcp=False):
+
+def _dnsQuery(hostname, serveListr='system', tcp=False):
     u'''纯粹的查询，并没有过滤之类的功能
 
 server:
@@ -132,47 +149,49 @@ server:
     '''
     if serveListr == 'system':
         try:
-            res = socket.getaddrinfo(hostname, 80,0,socket.SOCK_STREAM,socket.IPPROTO_TCP)
+            res = socket.getaddrinfo(hostname, 80, 0, socket.SOCK_STREAM, socket.IPPROTO_TCP)
             return [r[4][0] for r in res]
         except Exception:
             info = traceback.format_exc()
-            logging.debug(u'[DNS][_dnsQuery][socket.getaddrinfo] 解析失败，host=%s 详细信息：'%hostname)
-            logging.debug('%s\r\n\r\n'%info)
+            logging.debug(u'[DNS][_dnsQuery][socket.getaddrinfo] 解析失败，host=%s 详细信息：' % hostname)
+            logging.debug('%s\r\n\r\n' % info)
             return []
     else:
-        t = dnsPool.spawn(_dnspythonQuery,hostname,serveListr,tcp)
+        t = dnsPool.spawn(_dnspythonQuery, hostname, serveListr, tcp)
         return t.get(True)
 
-def _dnspythonQuery(hostname,serveListr,tcp=False):
+
+def _dnspythonQuery(hostname, serveListr, tcp=False):
     try:
         res = []
         m = dns.resolver.Resolver()
 
-        if isinstance(serveListr,(str,unicode)):
-            serveListr = [serveListr,]
+        if isinstance(serveListr, (str, unicode)):
+            serveListr = [serveListr, ]
 
-        m.nameservers=serveListr
+        m.nameservers = serveListr
 
-        answerList = m.query(hostname,tcp=tcp).response.answer
+        answerList = m.query(hostname, tcp=tcp).response.answer
         for a in answerList:
             for r in a:
                 res.append(r.address)
         return res
-    except :
+    except:
         info = traceback.format_exc()
-        logging.debug(u'[DNS][_dnspythonQuery][dns.resolver.query] 解析失败，host=%s ,nameserver=%s详细信息：'%(hostname,serveListr))
-        logging.debug('%s\r\n\r\n'%info)
+        logging.debug(
+            u'[DNS][_dnspythonQuery][dns.resolver.query] 解析失败，host=%s ,nameserver=%s详细信息：' % (hostname, serveListr))
+        logging.debug('%s\r\n\r\n' % info)
         return []
+
 
 def dnsQueryLoop():
     while True:
-        global  errIP
-        global  configIpBlacklist
-        _errIP ={}
+        global errIP
+        global configIpBlacklist
+        _errIP = {}
 
         for ip in configIpBlacklist:
-            _errIP[ip]=-1
-
+            _errIP[ip] = -1
 
         if not errIP:
             errIP.update(_errIP)
@@ -182,33 +201,32 @@ def dnsQueryLoop():
         # 统计所有的 DNS 服务器
         allDnsServer = set()
 
-        if isinstance(nameservers,(str,unicode)):
+        if isinstance(nameservers, (str, unicode)):
             allDnsServer.add(nameservers)
         else:
             allDnsServer.update(nameservers)
 
-        if isinstance(nameserversBackup,(str,unicode)):
-            allDnsServer.add(nameserversBackup)
+        if isinstance(nameservers_backup, (str, unicode)):
+            allDnsServer.add(nameservers_backup)
         else:
-            allDnsServer.update(nameserversBackup)
+            allDnsServer.update(nameservers_backup)
 
         for i in range(3):
             for dnsServer in allDnsServer:
-                ipList = _dnsQuery("sdfagdfkjvgsbyeastkisbtgvbgkjscabgfaklrv%s.com"%i,dnsServer)
+                ipList = _dnsQuery("sdfagdfkjvgsbyeastkisbtgvbgkjscabgfaklrv%s.com" % i, dnsServer)
                 for ip in ipList:
-                    logging.info(u'[DNS]采集到域名服务器(%s)域名纠错IP(%s)。' % (dnsServer,ip))
-                    _errIP[ip] = int(time.time()*1000)
-
+                    logging.info(u'[DNS]采集到域名服务器(%s)域名纠错IP(%s)。' % (dnsServer, ip))
+                    _errIP[ip] = int(time.time() * 1000)
 
         if len(errIP) == len(configIpBlacklist):
             errIP.update(_errIP)
 
         for i in range(100):
-            ipList = _dnsQuery('twitter.com',['8.8.8.234','8.8.8.123',])
+            ipList = _dnsQuery('twitter.com', ['8.8.8.234', '8.8.8.123', ])
             for ip in ipList:
-                logging.info(u'[DNS]采集到远程异常IP(%s)。'%ip)
-                _errIP[ip] = int(time.time()*1000)
-            if i%10 == 0:
+                logging.info(u'[DNS]采集到远程异常IP(%s)。' % ip)
+                _errIP[ip] = int(time.time() * 1000)
+            if i % 10 == 0:
                 errIP.clear()
                 errIP.update(_errIP)
             gevent.sleep(1)
@@ -218,6 +236,6 @@ def dnsQueryLoop():
 
         logging.info(u'[DNS]采集到的所有异常IP为：\r\n' + '\r\n'.join(errIP))
 
-        gevent.sleep(1*60*60)
+        gevent.sleep(
+* 60 * 60)
 
-"""
--- upstream/direct.py
+++ upstream/direct.py
@@ -6,9 +6,13 @@ import traceback
 from gevent import socket as _socket
 import gevent
 import time
+from gevent.event import Event
 from gevent.pool import Group
+import math
+from LRUCacheDict import LRUCacheDict
 
-from base import UpstreamBase
+from base import UpstreamBase, UpstreamConnectError
+import dnslib
 from mysocket import SocketBase
 
 
@@ -24,6 +28,8 @@ class DirectUpstream(UpstreamBase):
 
         self.source_ip = config.get('source_ip','0.0.0.0')
         self.source_port = config.get('source_port',0)
+
+        self.route_cache = LRUCacheDict(500, 10 * 60 * 1000)
 
         if self.source_ip == '0.0.0.0' and self.source_port==0:
             self.source_address = None
@@ -48,11 +54,120 @@ class DirectUpstream(UpstreamBase):
     def get_name(self):
         return '%s?source=%s&source_port=%s' % (self.type,self.source_ip,self.source_port)
 
-    def create_connection(self,address, timeout=10):
-        # TODO: 这里需要记录下本sock连接远程的耗时。
-        # TODO: 需要实现多ip同时连接的功能。
+    def get_route_order_ping(self, hostname, port, default=None):
+        route = self.get_route_cache(hostname, port, None)
+        if route:
+            return sorted(route.values(), key=lambda x: x['tcp_ping'])
+        return None
 
-        _sock = self.upstream.create_connection(address,timeout,self.source_address)
+    def get_route_cache(self, hostname, port, default=None):
+        return self.route_cache.get('%s-%s' % (hostname, port), default)
+
+    def __set_route_cache(self, hostname, port, value):
+        self.route_cache['%s-%s' % (hostname, port)] = value
+
+    def update_route_ping(self,  hostname, port, ping, ip):
+        proxyDict = self.get_route_cache(hostname, port)
+        if proxyDict == None:
+            proxyDict = {}
+            self.__set_route_cache(hostname, port, proxyDict)
+
+        proxyDict['%s' % ( ip)] = {
+            'tcp_ping': ping,
+            'hit_ip': ip
+        }
+
+    def _direct_create_connection(self,address,ip, timeout=10):
+        # 实际连接
+        _sock = self.upstream.create_connection((ip,address[1]),timeout,source_address=self.source_address)
         sock = self.socket(_sock=_sock)
         _sock.setsockopt(_socket.IPPROTO_TCP,_socket.TCP_NODELAY, 1)
         return sock
+
+    def _create_connection(self, aync_task, address,ip, timeout=10):
+        # 多线程连接执行部分
+        start_time = int(time.time() * 1000)
+        try:
+            sock = self._direct_create_connection(address,ip, timeout)
+        except:
+            t = int(time.time() * 1000) - start_time
+            info = traceback.format_exc()
+            logging.debug(
+                u'[upstream]%s 连接 %s(%s):%s 失败。time:%s' % (self.get_display_name(), address[0],ip, address[1], t))
+            logging.debug('%s\r\n\r\n' % info)
+            return
+        t = int(time.time() * 1000) - start_time
+        self.update_route_ping(address[0],address[1],t,ip)
+        if aync_task.sock:
+            sock.close(safe=False)
+            logging.debug(
+                u'[upstream]%s 连接 %s(%s):%s 未命中。time:%s' % (self.get_display_name(), address[0],ip, address[1], t))
+        else:
+            aync_task.sock = sock
+            aync_task.evt.set()
+            logging.debug(
+                u'[upstream]%s 连接 %s(%s):%s 命中。time:%s' % (self.get_display_name(), address[0],ip, address[1], t))
+
+    def _create_connection_all_end(self, aync_task):
+        u"""在所有链接全部出错时发出事件通知主协程。"""
+        aync_task.group.join()
+        if aync_task.sock is None:
+            aync_task.evt.set()
+
+    def create_connection(self, address, timeout=10):
+
+        ip_list = dnslib.dnsQuery(address[0])
+
+        # 尝试连接缓存
+        route_list = self.get_route_order_ping(address[0],address[1],None)
+        if route_list:
+            try:
+                route = route_list[0]
+                hit_ip = route['hit_ip']
+
+                if hit_ip in ip_list:
+                    cache_timeout = route['tcp_ping']
+
+                    if cache_timeout<1000:
+                        cache_timeout = cache_timeout * 2
+                    else:
+                        cache_timeout = cache_timeout+1000
+                    cache_timeout = int(math.ceil(cache_timeout/1000.0))
+
+                    start_time = int(time.time() * 1000)
+                    sock = self._direct_create_connection(address,hit_ip, cache_timeout)
+                    t = int(time.time() * 1000) - start_time
+                    logging.debug(u'[upstream][RouteCache]%s 缓存记录 连接 %s(%s):%s 命中。time:%s'%(self.get_display_name(),address[0],hit_ip,address[1],t))
+                    self.update_route_ping(address[0],address[1],t,hit_ip)
+                    return sock
+                else:
+                    logging.debug(u'[upstream][RouteCache]%s 缓存记录 连接 %s(%s):%s IP 不匹配，放弃缓存。'%(self.get_display_name(),address[0],hit_ip,address[1]))
+            except:
+                t = int(time.time() * 1000) - start_time
+                info = traceback.format_exc()
+                logging.debug(
+                    u'[upstream][RouteCache]%s 缓存记录 连接 %s(%s):%s 失败。time:%s' % (self.get_display_name(), address[0],hit_ip, address[1],t))
+                logging.debug('%s\r\n\r\n' % info)
+
+        # 缓存失败，连接全部
+        evt = Event()
+        group = Group()
+        aync_task = DirectAsyncTask(evt, None, group)
+
+        for ip in ip_list:
+            group.add(gevent.spawn(self._create_connection,  aync_task, address,ip, timeout))
+
+        # 所有连接失败时发出通知
+        gevent.spawn(self._create_connection_all_end, aync_task)
+
+        evt.wait()
+        if aync_task.sock:
+            return aync_task.sock
+        else:
+            raise UpstreamConnectError()
+
+class DirectAsyncTask():
+    def __init__(self, evt, sock, group):
+        self.evt = evt
+        self.sock = sock
+        self.group = group
